b322a6b28ef9392c54fe1c65c817ea51981b7712
tofarati.agbaje <tofarati.agbaje@gmail.com>
2020-05-04 17:09:53 +0200

    Scaffolding and Routing

---
+++ src/components/Chat.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Chat extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Chat
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Chat;
---
+++ src/components/FileShare.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class FileShare extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          File Sharing
+        </p>
+      </div>
+    );
+  }
+}
+
+export default FileShare;
